5d238824497f1a71412e540bbbaf3064169a71d0
msfArmando <armandomonsaof@gmail.com>
2023-02-02 17:09:06 -0300

    Implementação de Interfaces

--- ByBank/Sistemas/SistemaInterno.cs
+++ ByBank/Sistemas/SistemaInterno.cs
@@ -9,7 +9,7 @@ namespace ByBank.Sistemas
 {
     internal class SistemaInterno
     {
-        public bool Logar(Autenticavel funcionario, string senha)
+        public bool Logar(IAutenticavel funcionario, string senha)
         {
             bool usuarioAutenticado = funcionario.Autenticar(senha);
 
